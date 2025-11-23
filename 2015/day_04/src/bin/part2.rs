de67fd37ccc0ea09a8aac17e7f31004399edb92b
Kradse <kradse@gmail.com>
2025-11-23 22:26:00 +0100

    day 5

---
+++ 2015/day_04/src/bin/part2.rs
@@ -0,0 +1,20 @@
+use md5;
+
+fn main() {
+    let secret = "yzbqklnj";
+
+    let mut searching = true;
+    let mut index = 0;
+    while searching {
+        index += 1;
+
+        let value = format!("{}{}", secret, index);
+        let converted = md5::compute(value.clone());
+        let hex = format!("{:?}", converted);
+
+        if hex.starts_with("000000") {
+            println!("Found: {:?}", value.clone());
+            searching = false;
+        }
+    }
+}
